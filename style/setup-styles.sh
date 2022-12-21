99bbd41cae245a69f8289086927f532893b087f1
Sudeep Prasad <sudeep.rp@numocity.com>
2022-12-21 11:48:53 +0530

    add accept

--- style/setup-styles.sh
+++ style/setup-styles.sh
@@ -3,13 +3,14 @@ set -e
 
 npm install -g jscpd markdownlint-cli markdown-link-check
 
-mkdir vale
+mkdir -p vale
 cd vale
 wget https://github.com/errata-ai/vale/releases/download/v2.15.2/vale_2.15.2_Linux_64-bit.tar.gz
 tar -xf 'vale_2.15.2_Linux_64-bit.tar.gz'
 ./vale -v
 cd -
 
+mkdir -p .github/styles/
 cd .github/styles/
 wget https://github.com/errata-ai/Microsoft/releases/latest/download/Microsoft.zip
 unzip Microsoft.zip
